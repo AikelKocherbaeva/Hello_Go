6ea18e9fcd97a435b265789fd8da77d0fab729c7
AikelKocherbaeva <80120596+AikelKocherbaeva@users.noreply.github.com>
2021-09-27 16:28:23 +0600

    Add Create func

--- main.go
+++ main.go
@@ -1,19 +1,23 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	handleFunc()
-
 }
 
 func handleFunc() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
 	http.HandleFunc("/", index)
+	http.HandleFunc("/create", create)
+	http.HandleFunc("/save_article", save_article)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -24,4 +28,35 @@ func index(w http.ResponseWriter, r *http.Request) {
 	}
 
 	t.ExecuteTemplate(w, "index", nil)
+}
+func create(w http.ResponseWriter, r *http.Request) {
+	t, err := template.ParseFiles("templates/create.html", "templates/header.html", "templates/footer.html")
+	if err != nil {
+		fmt.Fprintf(w, err.Error())
+	}
+
+	t.ExecuteTemplate(w, "create", nil)
+}
+func save_article(w http.ResponseWriter, r *http.Request) {
+	title := r.FormValue("title")
+	anons := r.FormValue("anons")
+	full_text := r.FormValue("full_text")
+	if title == "" || anons == "" || full_text == "" {
+		fmt.Fprintf(w, "Не все данные заполенены")
+	} else {
+		db, err := sql.Open("mysql", "root:123@tcp(127.0.0.1:3306)/golang")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		// Установка данных
+		insert, err := db.Query(fmt.Sprintf("INSERT INTO `articles`(`title`, `anons`, `full_text`) VALUES ('%s', '%s', '%s')", title, anons, full_text))
+		if err != nil {
+			panic(err)
+		}
+		defer insert.Close()
+
+		http.Redirect(w, r, "/", http.StatusSeeOther)
+	}
 }
